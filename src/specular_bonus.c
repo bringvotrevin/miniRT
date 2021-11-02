b3d0c1cc520a1f39dfd238d69601edf8bcc6eeca
YooJ-L <you2641@naver.com>
2021-11-02 18:13:54 +0900

    FIX : specular_light variable

--- src/specular_bonus.c
+++ src/specular_bonus.c
@@ -6,7 +6,7 @@
 /*   By: yoojlee <yoojlee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 20:29:42 by yoojlee           #+#    #+#             */
-/*   Updated: 2021/11/01 20:35:01 by yoojlee          ###   ########.fr       */
+/*   Updated: 2021/11/02 18:12:42 by yoojlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ t_vec	specular_light(t_light *light, t_hit *hit)
 
 	l = minus_vec(light->origin, hit->point);
 	n = hit->normal;
-	n = product_scalar(n, 2 * dot_vec(l, n));
-	reflect_vec = minus_vec(n, l);
+	reflect_vec = product_scalar(n, 2 * dot_vec(l, n));
+	reflect_vec = minus_vec(reflect_vec, l);
 	angle = dot_vec(unit_vec(reflect_vec),
 			unit_vec(product_scalar(hit->dir, -1)));
 	if (angle < 0)
